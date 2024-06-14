8b4610745e589f5c1c57ba844ff0104cac0338ab
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-14 15:10:01 +0530

    Update info.py

--- MahakMusic/plugins/misc/info.py
+++ MahakMusic/plugins/misc/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/mahakxbot?startgroup=true"),
     ],
 ]
 
